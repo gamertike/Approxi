76511a3fce0bb12b5d722d682c2ae783444edb1d
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-03-22 21:52:45 +0000

    Fixed Event builder giving incorrect intelisense.

--- packages/approxi/src/builders/Event.ts
+++ packages/approxi/src/builders/Event.ts
@@ -4,22 +4,19 @@ import { ClientEvents, EventStructure } from '..';
  * The event builder.
  */
 export class Event<K extends keyof ClientEvents = keyof ClientEvents> {
-    /**
-     * The event name.
-     */
-    public name;
-    /**
-     * Whether the event should run once.
-     */
+	/**
+	 * The event name.
+	 */
+	public name;
+	/**
+	 * Whether the event should run once.
+	 */
 	public once;
 
 	/**
 	 * @param run The function to run when the evemt is emitted.
 	 */
-	constructor(
-		{ name, once = false }: Omit<EventStructure, 'run'>,
-		public run: (...args: ClientEvents[K]) => any
-	) {
+	constructor({ name, once = false }: Omit<EventStructure<K>, 'run'>, public run: (...args: ClientEvents[K]) => any) {
 		this.name = name;
 		this.once = once;
 	}
